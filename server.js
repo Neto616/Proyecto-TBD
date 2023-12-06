8b35db8d95c92df0525b2da595760f7bfa9df2c8
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2023-12-05 20:09:32 -0600

    Views Partials

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app=express();
 app.set('view engine','hbs');
 app.set('views',path.join(__dirname,'./views'));
 hbs.registerPartials(path.join(__dirname, `./views`));
+hbs.registerPartials(path.join(__dirname, `./views/partials`));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'./assets')));
